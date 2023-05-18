388352cd567e80c88ddadc97cae2a4bfcee01f1a
Danijel <93523375+danijelmiklosi01@users.noreply.github.com>
2023-05-18 20:37:12 +0200

    Povezivanje login prozora sa bazom, kreiranje nova dva prozora kuvar i magacioner radi verifikacije korisnika preko RoleID.

--- UniverOrganaProject/Windows/Login.xaml.cs
+++ UniverOrganaProject/Windows/Login.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using UniverOrganaProject.Windows;
 
 namespace UniverOrganaProject
 {
@@ -24,6 +26,75 @@ namespace UniverOrganaProject
         {
             InitializeComponent();
         }
+
+        private void BtnLogin(object sender, RoutedEventArgs e)
+        {
+            SqlConnection sqlCon = new SqlConnection(@"Data Source=DANIJEL; Initial Catalog=UniverOrgana; Integrated Security=True");
+            try
+            {
+                if (sqlCon.State == System.Data.ConnectionState.Closed)
+                    sqlCon.Open();
+                String query = "SELECT RoleID FROM tblUser WHERE Username=@Username AND Password=@Password";
+                SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
+                sqlCmd.CommandType = System.Data.CommandType.Text;
+                sqlCmd.Parameters.AddWithValue("@Username", txtUsername.Text);
+                sqlCmd.Parameters.AddWithValue("@Password", txtPassword.Password);
+                object result = sqlCmd.ExecuteScalar();
+
+                if (result != null && result != DBNull.Value)
+                {
+                    int roleId = Convert.ToInt32(result);
+
+                    if (roleId == 1)
+                    {
+                        Magacioner magacionerWindow = new Magacioner();
+                        magacionerWindow.Show();
+                    }
+                    else if (roleId == 2)
+                    {
+                        Kuvar kuvarWindow = new Kuvar();
+                        kuvarWindow.Show();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Nemate pristup ovom sistemu.");
+                        return; // Prekidamo dalje izvršavanje ako korisnik nema pristup
+                    }
+
+                    Close(); // Zatvaranje trenutnog prozora za logovanje
+                }
+                else
+                {
+                    MessageBox.Show("Korisničko ime ili lozinka su netačni.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                sqlCon.Close();
+            }
+        }
+
+        private void Txt_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                // Provera kojem elementu je fokusiran taster Enter
+                if (sender == txtUsername)
+                {
+                    txtPassword.Focus(); // Prebacivanje fokusa na polje za unos lozinke
+                }
+                else if (sender == txtPassword)
+                {
+                    BtnLogin(sender, e); // Izvršavanje logovanja ako je fokus na polju za unos lozinke
+                }
+            }
+        }
+
+
 
         private void MinimizeWindow(object sender, RoutedEventArgs e)
         {
